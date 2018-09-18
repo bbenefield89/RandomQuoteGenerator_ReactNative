4ee327af542d82efd7aa85a2a39ae525482cea47
bbenefield89 <bsquared18@gmail.com>
2018-09-17 21:02:31 -0400

    MAKE XHR CALL TO GRAB A RANDOM QUOTE AND PASS TO <Quote />
    - in components/QuoteContainer/QuoteContainer.js

--- components/QuoteContainer/QuoteContainer.js
+++ components/QuoteContainer/QuoteContainer.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import axios from 'axios'
+
 import { Button, View, Text } from 'react-native'
 
 import Quote from './modules/Quote'
@@ -9,27 +11,48 @@ class QuoteContainer extends Component {
     super(props);
     this.state = {
       quoteData: {
-        quoteBody: '',
-        quoteAuthor: ''
+        quote: '',
+        author: ''
       },
       tweetURL: 'https://twitter.com/intent/tweet?hashtags=quotes&text='
     }
   }
 
+  // getRandomQuote
   getRandomQuote = () => {
     const quoteURL = 'https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json'
+
+    axios(quoteURL)
+      .then(({ data }) => {
+        const { quotes }        = data
+        const max               = quotes.length
+        const rand              = Math.floor(Math.random() * Math.floor(max))
+        const randomQuote       = quotes[ rand ]
+        const { quote, author } = randomQuote
+        const quoteData         = { quote, author }
+        
+        this.setState({ quoteData })
+      })
+      .catch(err => console.log(err))
   }
   
+  // openURL
   openURL = url => {
     Linking.openURL('https://www.google.com')
   }
 
+  // componentDidMount
+  componentDidMount() {
+    this.getRandomQuote()
+  }
+  
+  // render
   render() { 
     return (
       <View>
         <Quote
-          quoteBody='Hello, world'
-          quoteAuthor='- Brandon Benefield'
+          quote={ this.state.quoteData.quote }
+          author={ this.state.quoteData.author }
         />
 
         <Button
@@ -38,7 +61,7 @@ class QuoteContainer extends Component {
         />
         
         <Button
-          onPress={ () => console.log('Hello, world!') }
+          onPress={ this.getRandomQuote }
           title='Generate new quote'
         />
       </View>
